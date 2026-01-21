8475c4ba145988ed8382715f4561afe7c82f7599
Jordi Orriols <info@jordiorriols.cat>
2026-01-21 15:06:05 +0100

    chore: remove not used code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import CodeVisualizerPage from "./CodeVisualizer";
+import Home from "./pages/home";
 
 export default () => (
   <>
-      <CodeVisualizerPage/>
+    <Home />
   </>
 );
